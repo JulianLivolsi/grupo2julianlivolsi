9868ea047c9055ca9823baaf6ba9bbbadc099212
EstebanPlanas <estebanramiroplanas@gmail.com>
2026-04-25 13:23:17 -0300

    main y jercicios.h

---
+++ 02-trabajoPractico-Listas/ejercicios_tp_2.h
@@ -0,0 +1,10 @@
+#ifndef EJERCICIOS_TP_2_INCLUDED
+#define EJERCICIOS_tp_2_INCLUDED
+
+int ejercicio2();
+int ejercicio3();
+int ejercicio4();
+int ejercicio5();
+int ejercicio6();
+
+#endif // EJERCICIO_H_INCLUDED
---
+++ 02-trabajoPractico-Listas/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tp_2_listas.h"
+#include "ejercicios_tp_2.h"
+
+
+int main()
+{
+    int eleccion = -1;
+    while (eleccion != 0)
+    {
+        limpiarConsola();
+        printf("\n------ Menu TP2: Listas ------\n");
+        eleccion = leer_entero("Seleccione el ejercicio a ejecutar [2-6] | [0] para salir: ", 0, 6);
+        while (eleccion == 1)
+        {
+            printf("Entrada inválida. Ingrese otro entero\n");
+            eleccion = leer_entero("Seleccione el ejercicio a ejecutar [2-6] | [0] para salir): ", 0, 6);
+        }
+        if (eleccion == 0)
+        {
+            printf("\nSaliendo del programa...\n");
+        }
+        else
+        {
+            switch (eleccion)
+            {
+            case 2:
+                ejercicio2();
+                break;
+            case 3:
+                ejercicio3();
+                break;
+            case 4:
+                ejercicio4();
+                break;
+            case 5:
+                ejercicio5();
+                break;
+            case 6:
+                ejercicio6();
+                break;
+            }
+        }
+    }
+    return 0;
+}
