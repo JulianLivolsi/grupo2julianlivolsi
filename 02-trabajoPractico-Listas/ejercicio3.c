a1ff732d2341d1317d716170e5a697adbb1eafa9
esnauj <jse.unlu@gmail.com>
2026-04-27 12:23:46 -0300

    Se corrigio el ejercicio 3 para utilizar Iteradores, se resolvio ejercicio 4

--- 02-trabajoPractico-Listas/ejercicio3.c
+++ 02-trabajoPractico-Listas/ejercicio3.c
@@ -8,29 +8,75 @@
 
 #include "tp_2_listas.h"
 
-// EJERCICIO 3
-
 ResultadosMul multiplo(Lista l1, Lista l2)
 {
-    ResultadosMul resultado;
+    ResultadosMul resultado; // Definimos el resultado base como false y escalar 0
     resultado.esMultiplo = false;
     resultado.escalar = false;
     resultado.numEscalar = 0;
 
-    if (l_es_vacia(l1) || l_es_vacia(l2)) return resultado;
+    if (l_es_vacia(l1) || l_es_vacia(l2)) return resultado; // Si alguna esta vacia retornamos el resultado base
+    if (l_longitud(l1) != l_longitud(l2)) return resultado; // Si son de distinta longitud retornamos el resultado base
 
-    int seguir = 1, i = 1;
-    while (seguir){
-        TipoElemento elemento = l_recuperar(l1, i);
-        TipoElemento elemento2 = l_recuperar(l2, i);
+    Iterador it1 = iterador(l1);
+    Iterador it2 = iterador(l2);
 
-        if (elemento->clave % elemento2->clave != 0){
-            seguir = 0;
+    bool todosMultiplos = true, esEscalar = true;
+    int k_base = 0;
+    bool primerElemento = true; 
+
+    while (hay_siguiente(it1) && hay_siguiente(it2))
+    {
+        TipoElemento e1 = siguiente(it1); // Sacamos los elementos y avanzamos el iterador un paso
+        TipoElemento e2 = siguiente(it2);
+
+        if (e2->clave == 0) // Verifica que no divida por 0
+        { 
+            if (e1->clave != 0) // Si la clave del otro elemento no es 0, no son multiplo
+            {
+                todosMultiplos = false;
+                break;
+            }
+            continue; 
+        }
+        if (e1->clave % e2->clave != 0) // Verifica si NO es multiplo
+        { 
+            todosMultiplos = false;
+            esEscalar = false;
+            break; 
+        }
+
+        int k_actual = e1->clave / e2->clave; // Verifica la logica del escalar
+
+        if (primerElemento)
+        {
+            k_base = k_actual; 
+            primerElemento = false; // Apagamos la bandera para las siguientes vueltas
+        }
+        else
+        {
+            if (k_actual != k_base)
+            {
+                esEscalar = false; 
+            }
         }
     }
+
+    if (todosMultiplos) {
+        resultado.esMultiplo = true;
+        resultado.escalar = esEscalar;
+        if (esEscalar) {
+            resultado.numEscalar = k_base;
+        }
+    }
+
+    free(it1); // Liberamos la memoria para los iteradores
+    free(it2);
+
+    return resultado;
 }
 
-void ejercicio3()
+/* void ejercicio3()
 {
     limpiarConsola();
     srand(time(NULL));
@@ -59,4 +105,45 @@ void ejercicio3()
     int continuar = pedirEntero("\nSalir (0) | Continuar (1): ", 0, 1);
     if (continuar == 1)
         ejercicio3();
+} */
+
+
+// Lo unico que cambie fue que en lugar de llamarse recursivamente, se use un do while para volverse a ejecutar hasta que el usuario salga, para usar menos memoria
+void ejercicio3()
+{
+    int continuar;
+    do
+    {
+        limpiarConsola();
+        srand(time(NULL));
+
+        printf("\n----- Ejercicio 3: Multiplo -----");
+        int largo = pedirEntero("Ingrese el largo de las listas (para que sean multiplo ambas tienen el mismo): ", 1, 100);
+
+        Lista l1 = rellenarLista(largo); // Esta funcion auxiliar usa l_crear y rellena los elementos con 
+        Lista l2 = rellenarLista(largo);
+
+        printf("\n### Lista 1: \n");
+        l_mostrar(l1);
+        printf("\n### Lista 2: \n");
+        l_mostrar(l2);
+
+        ResultadosMul resultadoDeMultiplos = multiplo(l1,l2);
+
+        if (resultadoDeMultiplos.esMultiplo)
+        {
+            printf("\nLas listas son multiplos entre si");
+            if (resultadoDeMultiplos.escalar)
+            {
+                printf(", y son multiplos por un escalar constante = %d\n", resultadoDeMultiplos.numEscalar);
+            } else
+            {
+                printf(", pero el multiplo no es constante (varia entre posiciones).\n");
+            }
+        } else
+        {
+            printf("\nLas listas no son multiplos entre si.\n");
+        }
+        continuar = pedirEntero("\nSalir (0) | Continuar (1): ", 0, 1);
+    } while (continuar == 1);
 }
--- 02-trabajoPractico-Listas/ejercicio4.c
+++ 02-trabajoPractico-Listas/ejercicio4.c
@@ -7,3 +7,35 @@
 
 #include "tp_2_listas.h"
 #include "../libs/validaciones/headers/AUXILIARES.h"
+
+// P4 Retorna 1 si L1 > L2, 2 si L2 > L1, 0 si son iguales
+int CompararListas(Lista l1, Lista L2)
+{
+    int mayoresL1 = 0, mayoresL2 = 0; // Crea contadores de mayores para cada lista | Complejidad 0(1)
+
+    Iterador it1 = iterador(l1); // Crea iteradores para cada lista | Complejidad O(1)
+    Iterador it2 = iterador(L2);
+
+    while (hay_siguiente(it1) && hay_siguiente(it2)) // Itera las listas mientras que AMBAS tengan siguiente elemento | Complejidad O(N)
+    {
+        TipoElemento e1 = siguiente(it1); // Sacamos los elementos y avanzamos el iterador un paso
+        TipoElemento e2 = siguiente(it2);
+
+        if (e1->clave > e2->clave) // Compara las claves y aumento el contador de la lista que tiene el mayor valor
+        {
+            mayoresL1++;
+        }
+        else if (e1->clave < e2->clave)
+        {
+            mayoresL2++;
+        }
+    }
+
+    free(it1); free(it2); // Libera la memoria de los iteradores
+
+    if (mayoresL1 > mayoresL2) return 1;
+    else if (mayoresL1 < mayoresL2) return 2;
+    else return 0;
+
+    // El algoritmo tiene complejidad O(N), debido al while que depende de la N cantidad de elementos de la lista mas corta
+}
