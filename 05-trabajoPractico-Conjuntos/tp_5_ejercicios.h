cc150854f47a9eed804c0a5cccc6ac79d617dcf5
Agustin327 <agustinmarcos369@outlook.com>
2026-06-21 14:05:34 -0300

    Update tp_5_ejercicios.h

--- 05-trabajoPractico-Conjuntos/tp_5_ejercicios.h
+++ 05-trabajoPractico-Conjuntos/tp_5_ejercicios.h
@@ -1,12 +1,12 @@
 #ifndef EJERCICIOS_TP_5_INCLUDED
 #define EJERCICIOS_TP_5_INCLUDED
  
-int ejercicio2();
-int ejercicio3();
-int ejercicio4();
-int ejercicio5();
-int ejercicio6();
-int ejercicio7();
-int ejercicio8();
+void ejercicio2();
+void ejercicio3();
+void ejercicio4();
+void ejercicio5();
+void ejercicio6();
+void ejercicio7();
+void ejercicio8();
  
 #endif // EJERCICIOS_TP_5_INCLUDED
